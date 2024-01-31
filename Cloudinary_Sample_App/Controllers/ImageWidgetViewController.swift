26e17351983ece523213e78b5f949be142e6e0bd
adimiz1 <adi.mizrahi@cloudinary.com>
2024-01-31 12:39:43 +0200

    Fix Image Widget module

--- Cloudinary_Sample_App/Controllers/ImageWidgetViewController.swift
+++ Cloudinary_Sample_App/Controllers/ImageWidgetViewController.swift
@@ -10,9 +10,29 @@ import UIKit
 import Cloudinary
 
 class ImageWidgetViewController: UIViewController {
+    @IBOutlet weak var ivLocal: CLDUIImageView!
+    @IBOutlet weak var ivRemote: CLDUIImageView!
+    @IBOutlet weak var ivCloudinary: CLDUIImageView!
 
+    var cloudinary = CLDCloudinary(configuration: CLDConfiguration(cloudName: "mobiledemoapp", secure: true))
 
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        setLocalImage()
+        setRemoteImage()
+        setCloudinaryImage()
+    }
+
+    private func setLocalImage() {
+        ivLocal.image = UIImage(named: "house")
+    }
+
+    private func setRemoteImage() {
+        ivRemote.cldSetImage("https://res.cloudinary.com/mobiledemoapp/image/upload/v1706628181/Demo%20app%20content/Frame_871_ao5o4r.jpg", cloudinary: cloudinary)
+    }
+
+    private func setCloudinaryImage() {
+        ivCloudinary.cldSetImage(publicId: "Demo%20app%20content/Frame_871_ao5o4r", cloudinary: cloudinary)
     }
 }
